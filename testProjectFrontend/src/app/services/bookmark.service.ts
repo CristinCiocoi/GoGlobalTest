499c23311f526298a347cb4d09afb8428c8980ac
Cris <cristin.ciocoi@feel-it-services.com>
2022-07-29 22:52:02 +0300

    clear

--- testProjectFrontend/src/app/services/bookmark.service.ts
+++ testProjectFrontend/src/app/services/bookmark.service.ts
@@ -1,16 +1,14 @@
-﻿import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { IBookMark, IItem } from '../services/repository.service';
-import { Observable } from 'rxjs';
+﻿import { Injectable } from "@angular/core";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { IBookMark, IItem } from "../services/repository.service";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class BookMarkService {
-  displayStyle = 'none';
   constructor(private http: HttpClient) {}
 
   public AddBookMark(model: IItem) {
-    console.log(model);
-    return this.http.post('https://localhost:7261/api/BookMark', {
+    return this.http.post("https://localhost:7261/api/BookMark", {
       NameRepository: model.name,
       description: model.description,
       avatarUrl: model.owner.avatar_url,
@@ -22,26 +20,19 @@ export class BookMarkService {
   }
 
   public GetAllBookMarks() {
-    return this.http.get<IItem[]>('https://localhost:7261/api/BookMark');
+    return this.http.get<IItem[]>("https://localhost:7261/api/BookMark");
   }
 
   public ExportData(): Observable<HttpResponse<Blob>> {
-    return this.http.get('https://localhost:7261/api/BookMark/export', {
-      responseType: 'blob',
-      observe: 'response',
+    return this.http.get("https://localhost:7261/api/BookMark/export", {
+      responseType: "blob",
+      observe: "response",
     });
   }
 
   public SendMessage(email: string) {
-    return this.http.post('https://localhost:7261/api/BookMark/send-msg', {
+    return this.http.post("https://localhost:7261/api/BookMark/send-msg", {
       email: email,
     });
-  }
-
-  openPopup() {
-    this.displayStyle = 'block';
-  }
-  closePopup() {
-    this.displayStyle = 'none';
   }
 }
